d5112aa580cf1f0cb0c00688d69a2c47587d424d
SethSenpai <pimwing@gmail.com>
2016-08-26 20:21:44 +0200

    nightly backup

---
+++ Assets/_scripts/buttonScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class buttonScript : MonoBehaviour {
+
+    public GameObject popupwindow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void mainMenu()
+    {
+        popupwindow.SetActive(true);
+    }
+
+    public void mainMenuYes()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void mainMenuNo()
+    {
+        popupwindow.SetActive(false);
+    }
+}
